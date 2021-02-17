f43e4efa50933793900986f0fb326ea4dc86b900
munozjuan <munoz.juan07@hotmail.com>
2021-02-16 23:53:45 -0500

    added schema

--- models/Workout.js
+++ models/Workout.js
@@ -6,8 +6,30 @@ const workoutSchema = new Schema ({
     name: {
         type: String,
     },
-
+    type: {
+        type: String,
+        trim: true
+    },
+    duration: {
+        type: Number
+    },
+    weight: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    },
+    duration: {
+        type: Number
+    },
+    distance: {
+        type: Number
+    }
 });
+
 
 
 const Workout = mongoose.model("Workout", workoutSchema);
