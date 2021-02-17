d0455970e978643e9e89a894d1f109afa026af91
munozjuan <munoz.juan07@hotmail.com>
2021-02-16 22:43:14 -0500

    added router api

--- models/Workout.js
+++ models/Workout.js
@@ -3,5 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema ({
+    name: {
+        type: String,
+    },
 
 });
--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,14 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout");
+
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .sort({_id: -1})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
