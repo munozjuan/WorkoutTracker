c5d355c5cd2f65318148c3ce81fa1781b3378ce2
munozjuan <munoz.juan07@hotmail.com>
2021-02-16 21:51:49 -0500

    added server.js

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
