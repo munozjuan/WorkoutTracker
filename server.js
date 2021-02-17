0e3c47bf89ba2ad47dd1380008cd05cc970c5bec
munozjuan <munoz.juan07@hotmail.com>
2021-02-16 21:58:21 -0500

    added routes on server.js

--- server.js
+++ server.js
@@ -7,3 +7,17 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+//routes
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
